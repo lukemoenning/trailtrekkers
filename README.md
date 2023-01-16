643398ea3543c34c9dda6c397c648dc918afb6f4
lukemoenning <luke.moenning@gmail.com>
2023-01-15 20:49:35 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# trailtrekkers
+
+
+## todo
+- my hikes / profile
+- friends hikes
+- discover page
+- map page
+- way to display total miles hiked (potentially on the home tab)
---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+/**
+ * App component
+ */
+
+import React from "react";
+import styled from "styled-components";
+
+
+const Test = styled.div`
+  color: red;
+`;
+
+function App() {
+  return (
+    <Test>
+      <h1>test</h1>
+    </Test>
+  );
+}
+
+export default App;
